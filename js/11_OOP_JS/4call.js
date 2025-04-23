6547de1f0073f0728a37755b088c9d2d62c21bed
TheResilientCoder <syedasim2021@gmail.com>
2025-04-23 06:41:25 +0500

    calls  in JS

---
+++ js/11_OOP_JS/4call.js
@@ -0,0 +1,19 @@
+const SetUsername  =  function (username){
+    // Complex DB call
+    this.username = username;
+    console.log("SetUsername Called");
+    
+}
+
+
+const createUsername = function (name , emil, password){
+SetUsername.call(this ,name) //is ka siraf refrence giya  hay call horaha hain 
+// lakin Reference nahi araha jo arguments aur parameters hain woh 
+// so thats why we .call(this,name) means current context ka this lay lo
+  this.email = emil;
+  this.password = password;
+}
+
+
+const chai = new  createUsername("Asim" , "syedasim2021@gmail.com" ,"12!@!12")
+console.log(chai);
